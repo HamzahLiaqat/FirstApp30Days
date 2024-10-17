48df4b07a7abb192c93aa5b39abe7af18ff3eb6b
Hamzah Liaqat <hamzahliaqat353@gmail.com>
2024-10-17 22:50:51 +0500

    Day4Practicising

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart ';
 import 'package:practicise/pages/home_page.dart';
 import 'package:practicise/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(Practicise());
@@ -13,9 +14,13 @@ class Practicise extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         //  home: HomePage(),
-        themeMode: ThemeMode.dark,
-        theme: ThemeData(primarySwatch: Colors.indigo),
-        darkTheme: ThemeData(brightness: Brightness.dark),
+        // themeMode: ThemeMode.dark,
+        theme: ThemeData(
+            primarySwatch: Colors.indigo,
+            fontFamily: GoogleFonts.lato().fontFamily),
+
+        // darkTheme: ThemeData(brightness: Brightness.dark),
+        initialRoute: "/login",
         routes: {
           "/": (context) => HomePage(),
           "/login": (context) => LoginPage(),
